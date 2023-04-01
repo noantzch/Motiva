6906e301698d7498ad05885f47fddd037becb96c
Gianluca Tridente <tridentegianluca03@gmail.com>
2023-03-31 21:45:07 -0300

    falta responsive

--- src/App.js
+++ src/App.js
@@ -4,16 +4,11 @@ import Footer from "./components/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import QuienesSomos from "./components/QuienesSomos";
 import Capacitaciones from "./components/Capacitaciones";
-<<<<<<< HEAD
 import Servicios from "./components/servicios";
 import Contacto from "./components/contacto";
 import { Bienvenida } from "./components/bienvenida";
 
-=======
-import { Bienvenida } from "./components/bienvenida";
-import Servicios from "./components/servicios";
-import Contacto from "./components/contacto";
->>>>>>> ab82b325b377f81d25f81f6ba1f94c562c23cc33
+
 function App() {
   return (
     <BrowserRouter>
@@ -25,19 +20,11 @@ function App() {
         </header>
         <main>
           <Routes>
-<<<<<<< HEAD
             <Route path="/" element={<Bienvenida />} />
             <Route path="/quienes-somos" element={<QuienesSomos />} />
             <Route path="/capacitaciones" element={<Capacitaciones />} />
             <Route path="/terapias" element={<Servicios />} />
             <Route path="/contacto" element={<Contacto />} />
-=======
-          <Route path="/" element={<Bienvenida />} />
-            <Route path="/quienes-somos" element={<QuienesSomos />} />
-            <Route path="/capacitaciones" element={<Capacitaciones />} />
-            <Route path="/servicios" element ={<Servicios />} />
-            <Route path="/contacto" element ={<Contacto />} />
->>>>>>> ab82b325b377f81d25f81f6ba1f94c562c23cc33
           </Routes>
         </main>
         <footer>
--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import React from "react";
-=======
-import React from 'react';
->>>>>>> ab82b325b377f81d25f81f6ba1f94c562c23cc33
 import img1 from "./images/image2.png";
 import img2 from "./images/image3.png";
 import img3 from "./images/image4.png";
@@ -11,7 +7,6 @@ import img5 from "./images/image7.png";
 
 const CarouselMotiva = () => {
   return (
-<<<<<<< HEAD
     <div className="portadaContainer">
       <div
         id="carouselExampleInterval"
@@ -100,46 +95,3 @@ const CarouselMotiva = () => {
 };
 
 export default CarouselMotiva;
-=======
-    <div className='portadaContainer'>
-              <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
-          <div className="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
-          </div>
-
-          <div className="carousel-inner">
-            <div className="carousel-item active" data-bs-interval="2000">
-              <img src={img1} className="d-block w-100" alt="..."></img>
-            </div>
-            <div className="carousel-item" data-bs-interval="2000">
-              <img src={img2} className="d-block w-100" alt="..."></img>
-            </div>
-            <div className="carousel-item" data-bs-interval="2000">
-              <img src={img3} className="d-block w-100" alt="..."></img>
-            </div>
-            <div className="carousel-item" data-bs-interval="2000">
-              <img src={img4} className="d-block w-100" alt="..."></img>
-            </div>
-            <div className="carousel-item" data-bs-interval="2000">
-              <img src={img5} className="d-block w-100" alt="..."></img>
-            </div>
-          </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"  data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </button>
-        </div>
-      </div> 
-  )
-}
-
-export default CarouselMotiva
->>>>>>> ab82b325b377f81d25f81f6ba1f94c562c23cc33
