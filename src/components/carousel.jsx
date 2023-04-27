5512bb1300b9bed14a01c290c0518eb06817f97c
Luis Noel Antezana Chinchilla <109741521+noantzch@users.noreply.github.com>
2023-04-27 12:26:42 -0300

    Update carousel.jsx

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import img2 from "./images/image3.png";
-import img3 from "./images/image4.png";
+import img3 from "./images/image7.png";
 import img4 from "./images/image5.png";
 import img5 from "./images/image2.png";
 import img6 from "./images/image6.png";
